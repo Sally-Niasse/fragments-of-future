4f1c2723223948cda7362af8933d77511beb24c8
Sally-Niasse <79103543+Sally-Jackson@users.noreply.github.com>
2022-03-31 01:02:58 +0200

    maj bulle de choix

--- api_dialogue.php
+++ api_dialogue.php
@@ -11,13 +11,15 @@
    $id_suivant=0;
 
   if (isset($_GET['id_suivant'])){
-    //Première requête : récupération des infos concernant l'id  et mise en forme
+    //Première requête : récupération des infos concernant l'id +mise en forme
   $sql1 = "SELECT texte, position, pdp, perso, id_recit, suivant.preced AS id_preced, suivant.suiv FROM recit LEFT OUTER JOIN suivant ON recit.id_recit = suivant.preced WHERE id_recit =". $_GET['id_suivant'] . "";
 
+    // On prépare la requête avant l'envoi :
     $req = $bdd -> prepare($sql1);
     $req -> execute();
+    // On crée une liste non numérotée avec les résultats
     $data = $req -> fetchAll(PDO::FETCH_ASSOC);
-
+    // Initialisation des variables de texte et photo réutiliser dans les bulles
     $pos=  $data[0]['position'];
     $text= $data[0]['texte'];
     $id= $data[0]['id_recit'];
@@ -25,7 +27,6 @@
     $perso= $data[0]['perso'];
     $id_suivant = $_GET['id_suivant'];
     
-    //Selon le type de récit on change sa forme
     if($pos == "didascalie" || $pos == "chapter"){
       $texte_complet="<div class=".$pos." id=".$id." data-suiv=".$data[0]['suiv']."><p>".$text."</p></div>";
     }else{
@@ -39,10 +40,12 @@
   $reqchoix -> execute();
     if($reqchoix->rowcount()>1){ 
       foreach($reqchoix as $choix){
-        $choix_contenu.= "<div class='content'><p><a class='choice' href='#'id=".$choix["id_recit"] ." data-suiv='".$choix["suiv"]."'>" . $choix["texte"] . "</a></p></div>";
+        $choix_contenu.= "<div class='content'><p class='choice' id=".$choix["id_recit"] ." data-suiv='".$choix["suiv"]."'>" . $choix["texte"] . "</p></div>";
       }    
       $choix_complet="<div class ='bubbleChoice'>".$choix_contenu."</div>";
     }
+  // var_dump ($texte_complet);
+  // var_dump($id_suivant);
 
   // Troisième reqûete : Mise a jour de la sauvegarde du joueur en implémetant l'id_suivant dans la table 'save'
     $save="UPDATE utilisateurs SET save=".$id_suivant." WHERE id_utilisateurs=".$_SESSION["id_utilisateurs"]."";
@@ -51,10 +54,10 @@
 
 }
 
- //Mise a jour de la session de sauvegarde de l'utilisateur
+
 $_SESSION["save"]=$id_suivant;
+$return_arr = array($texte_complet, $choix_complet, $_SESSION["save"]);
 
 // Encoding array in JSON format
-$return_arr = array($texte_complet, $choix_complet, $_SESSION["save"]);
 echo json_encode($return_arr, JSON_UNESCAPED_SLASHES);
 ?>
